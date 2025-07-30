3c2fd2a4d1f3d965a986f9d761a3af6bf7161f75
Satyam Rai <114270778+Satyamrai8707@users.noreply.github.com>
2025-07-30 22:39:45 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://bookswap-backend-9szg.onrender.com',
+  baseURL: 'http://localhost:8800/api',
 });
 
 API.interceptors.request.use((req) => {
