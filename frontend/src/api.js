b0e3923653a85cca3ab13214e952b1c9fd8cd3e0
Satyam Rai <114270778+Satyamrai8707@users.noreply.github.com>
2025-07-30 22:18:53 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8800/api',
+  baseURL: 'https://backend-ovff.onrender.com',
 });
 
 API.interceptors.request.use((req) => {
